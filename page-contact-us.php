51a702cc66e896ba1685415d1b21ef0393a74874
Adam Grover <your_email@example.com>
2022-11-22 15:03:41 +0000

    Addd target=_blank to twitter share links

--- page-contact-us.php
+++ page-contact-us.php
@@ -41,7 +41,7 @@
                 <div class="content-header-social col-lg-6 col-md-12">
                     <h6 class="mb-0"><small>Share This Page</small></h6><span class="breadcrumb-spacer"></span>
                     <div class="content-area-social-links">
-                        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"><i class="fa-brands fa-twitter ps-2 fa-xl"></i></a>
+                        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" target="_blank" data-show-count="false"><i class="fa-brands fa-twitter ps-2 fa-xl"></i></a>
                         <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook-f ps-2 fa-xl"></i></a>
                         <a href="mailto:office@kowessex.co.uk"><i class="fa-solid fa-envelope ps-2 fa-xl"></i></a>   
 
